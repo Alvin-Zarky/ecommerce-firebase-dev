6268a4f8c11808a1ad5649881322b42abc3f1701
Hormenh-Lim <103828283+Alvin-Zarky@users.noreply.github.com>
2022-05-15 17:05:21 +0700

    web static ecommerce

---
+++ client/src/App.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import * as Routes from "./router"
+import Overview from './view/overview';
+import NotFound from "./view/not-found"
+import ShoppingCart from './view/shopping-carts';
+import SignIn from './view/sign-in';
+import ProductDetail from './view/product-detail';
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Switch>
+          <Route exact path={Routes.INDEX}>
+            <Overview />
+          </Route>
+          <Route path={Routes.CART}>
+            <ShoppingCart />
+          </Route>
+          <Route path={Routes.SIGN_IN}>
+            <SignIn />
+          </Route>
+          <Route path={`${Routes.PRODUCT}/:id`}>
+            <ProductDetail />
+          </Route>
+          <Route path={Routes.NOT_FOUND}>
+            <NotFound />
+          </Route>
+        </Switch>
+      </Router>
+    </>
+  );
+}
+
+export default App;
