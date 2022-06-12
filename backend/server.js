878398397530abd28cbba0e20c4f8598c8c646b5
Hormenh-Lim <103828283+Alvin-Zarky@users.noreply.github.com>
2022-06-12 10:09:31 +0700

    ecommerce-firebase-dev

--- backend/server.js
+++
@@ -1,9 +0,0 @@
-const express= require('express')
-const colors= require('colors')
-const config= require('dotenv').config()
-const app = express()
-const PORT= process.env.PORT || 8000
-
-const server= app.listen(PORT, () =>{
-  console.log(`Server is running on port ${PORT}`.cyan.bold.underline)
-})
